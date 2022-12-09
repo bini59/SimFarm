497e84152c80dbe86a8681fa625e374cd8973f92
brawlkim <rudgks3374@gmail.com>
2022-12-09 22:00:13 +0900

    AnimalModel
    최대한 구현이 가능할 때 까지 구현한거 같은데, 제가 보기에도 너무 빈약하고 그렇네요. 죄송합니다..

---
+++ SimFarm/Assets/Scripts/Model/AnimalModel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalModel : MonoBehaviourSingletonTemplate<AnimalModel>, IFarmAnimal, IBarnAnimal, IShopAnimal, IDayendAnimal, IResultAnimal
+{
+    private Animal[] animals;
+    private int index;
+
+    AnimalModel()
+    {
+        animals = new Animal[6];
+        index = 0;
+    }
+    public enum userInput
+    {
+        Feed,
+        useItem
+    }
+    public void buyAnimal()
+    {
+        animals[index++] = new Animal();
+    }
+    public void setAnimalState(userInput userinput , Animal.animalType animal)
+    {
+        index = (int)animal;
+        animals[index].setState((int)userinput);
+    }
+    public Animal[] getAnimalList()
+    {
+        return animals;
+    }
+
+    public Animal getAnimalState(Animal animal)
+    {
+        return animal;
+    }
+}
